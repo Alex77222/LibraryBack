29f68e3fc0325f1939f32ff5bb66e0c8ae613fa5
“Alex <korolenkosasha644@gmail.com>
2023-05-28 23:02:28 +0300

    Add User

---
+++ Library/Data/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Library.Data.Entities;
+
+public class User : IdentityUser
+{
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    public bool IsActive { get; set; }
+
+    public List<int>? Books { get; set; }
+
+    public Book? Book { get; set; }
+}
