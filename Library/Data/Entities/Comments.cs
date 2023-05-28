5dc5d31be90ae88e348b6d9a1a007b7c26137d1c
“Alex <korolenkosasha644@gmail.com>
2023-05-28 22:58:02 +0300

    Add Comments

--- Library/Data/Entities/Comments.cs
+++ Library/Data/Entities/Comments.cs
@@ -2,5 +2,13 @@
 
 public class Comments
 {
-    
+    public int Id { get; set; }
+
+    public string Text { get; set; } = string.Empty;
+
+    public DateTime Date { get; set; }
+
+    public int BookId { get; set; }
+
+    public Book? Book { get; set; }
 }
