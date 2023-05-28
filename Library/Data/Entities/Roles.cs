1d4718654aea4e142fafd526b739adfd31ae704d
“Alex <korolenkosasha644@gmail.com>
2023-05-28 22:59:35 +0300

    Add Roles

---
+++ Library/Data/Entities/Roles.cs
@@ -0,0 +1,10 @@
+﻿namespace Library.Data.Entities;
+
+public class Roles
+{
+    public static string Admin => "admin";
+
+    public static string Employee => "moderator";
+
+    public static string Accountant => "reader";
+}
