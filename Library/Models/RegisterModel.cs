9fd369aecf68790ba8a5a313c28a7af4c37cc86a
“Alex <korolenkosasha644@gmail.com>
2023-06-02 15:36:09 +0300

    Add LoginModel and RegisterModel

---
+++ Library/Models/RegisterModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Library.Models;
+
+public class RegisterModel
+{
+    public string UserName { get; set; } = string.Empty;
+
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+    
+    public string Password { get; set; } = string.Empty;
+}
