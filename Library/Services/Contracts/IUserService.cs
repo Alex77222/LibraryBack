fa828b8f691f938ba49f3657685ce45e3de968b7
“Alex <korolenkosasha644@gmail.com>
2023-06-06 21:05:04 +0300

    Add AddRolesAsync

---
+++ Library/Services/Contracts/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using Library.Models;
+
+namespace Library.Services.Contracts;
+
+public interface IUserService
+{
+    public Task<List<UserDto>> GetUsersAsync();
+
+    public Task<string> AddRolesAsync(string userName, List<string> roles);
+
+    public Task<UserDto> GetUserAsync(string userName);
+}
---
+++ Library/Services/UserService.cs
@@ -0,0 +1,36 @@
+﻿using Library.Data.Entities;
+using Library.Models;
+using Library.Services.Contracts;
+using Microsoft.AspNetCore.Identity;
+
+namespace Library.Services;
+
+public class UserService : IUserService
+{
+    private readonly UserManager<User> _userManager;
+    public Task<List<UserDto>> GetUsersAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<string> AddRolesAsync(string userName, List<string> roles)
+    {
+        var user = await _userManager.FindByNameAsync(userName);
+        if (user!=null)
+        {
+            var userRoles = await _userManager.GetRolesAsync(user);
+            var addedRoles =  roles.Except(userRoles);
+            var removedRoles = userRoles.Except(roles);
+            await _userManager.AddToRolesAsync(user, addedRoles);
+            await _userManager.RemoveFromRolesAsync(user, removedRoles);
+            return "Update roles successfully!";
+        }
+        
+        throw new Exception("User is not found");
+    }
+
+    public Task<UserDto> GetUserAsync(string userName)
+    {
+        throw new NotImplementedException();
+    }
+}
