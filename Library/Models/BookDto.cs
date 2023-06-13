068c1b05b5d80dda261642f73658db6a00058a27
“Alex <korolenkosasha644@gmail.com>
2023-06-14 00:26:23 +0300

    Add BookDto

---
+++ Library/Models/BookDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace Library.Models;
+
+public class BookDto
+{
+    public int  Id { get; set; }
+
+    public string Author { get; set; } = string.Empty;
+
+    public string? Description { get; set; }
+
+    public int Likes { get; set; }
+}
